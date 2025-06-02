795e7907d7d623391c5b08e29979fe8a544dd350
jhmoon2985 <jhmoon2985@gmail.com>
2025-06-02 13:57:59 +0900

    외부 접속 기본 설정

--- obj/Debug/net9.0/GPIMSWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/GPIMSWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GPIMSWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be9457b63622c0d1b8188e5115144fd334e3251")]
 [assembly: System.Reflection.AssemblyProductAttribute("GPIMSWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GPIMSWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
